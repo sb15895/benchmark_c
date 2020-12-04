b39866600763321464fa10bfb55799f8352fde5b
sb15895 <s203@nextgenio-login1.localdomain>
2020-12-04 16:42:44 +0000

    ignore

--- Source_files/bench_headerfiles.h
+++ Source_files/bench_headerfiles.h
@@ -4,3 +4,4 @@ double benchtick(int a);
 double benchtime(int a);
 void mpiiowrite(double* iodata, int n1, int n2, int n3, MPI_Comm cartcomm); 
 void serialwrite(double* iodata); 
+void hdf5write(double* iodata, int n1, int n2, int n3, MPI_Comm )
